03a21c338bd3140071536bc1e40fae9155ede6af
kgricour <kevin@newton.so>
2024-07-05 11:30:39 +0200

    fix arg -D

--- Formula/pico-tts.rb
+++ Formula/pico-tts.rb
@@ -7,8 +7,20 @@ class PicoTts < Formula
   def install
     system "git", "submodule", "init"
     system "git", "submodule", "update"
-    system "sed", "-i", "''","s|/usr/share/pico-tts/|#{prefix}/pico-tts/|", "pico-tts.c"
+    system "sed", "-i", "''", "s|/usr/share/pico-tts/|#{prefix}/pico-tts/|", "pico-tts.c"
+
+    system "sed", "-i", "''", "s/install -D/install/", "Makefile"
+    
     system "make"
+
+    mkdir_p "#{prefix}/usr/lib/"
+    mkdir_p "#{prefix}/usr/bin/"
+    mkdir_p "#{prefix}/usr/share/""
+
     system "make", "DESTDIR=#{prefix}/", "install"
+  end
+
+  test do
+    system "#{bin}/pico-tts", "--version"
   end
 end
