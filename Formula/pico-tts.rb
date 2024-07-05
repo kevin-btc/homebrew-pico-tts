f09348cb83453a3e621d9bdf8ded318cfaae6df3
kgricour <kevin@newton.so>
2024-07-05 10:52:56 +0200

    modif class name

--- Formula/pico-tts.rb
+++ Formula/pico-tts.rb
@@ -1,4 +1,4 @@
-class PicoTTS < Formula
+class PicoTts < Formula
   desc "lib and unix binary for text-to-speech engine from mac"
   homepage "https://github.com/kevin-btc/pico-tts"
   url "https://github.com/kevin-btc/pico-tts.git", tag: "0.1", using: :git
@@ -7,8 +7,8 @@ class PicoTTS < Formula
   def install
     system "git", "submodule", "init"
     system "git", "submodule", "update"
-    system "sed", "-i", "s|/usr/local/pico-tts/|#{prefix}/usr/local/pico-tts/|", "pico-tts.c"
- system "make"
- system "make", "DESTDIR=#{prefix}/", "install"
+    system "sed", "-i", "s|/usr/share/pico-tts/|#{prefix}/usr/local/pico-tts/|", "pico-tts.c"
+    system "make"
+    system "make", "DESTDIR=#{prefix}/", "install"
   end
 end
